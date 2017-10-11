3b5d89499fe7858cb487b7ee9154708af94c05c5
KevinT <kevin.tai.mail@gmail.com>
2017-10-11 16:18:35 -0700

    FizzBuzzV1

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -8,41 +8,48 @@
 
 #include <stdio.h>
 
+//main program returning integer
 int main(int argc, const char * argv[]) {
 
-    //multiple of 3 equal 0
+    //integer remainder of division by 3
     int j;
     
-    //multiple of 5 equal 0
+    //integer remainder of division by 5
     int k;
     
-    //evaluate every number from 1 to 100 (less than 101)
-    //i is each number being evaluated starting from 1 and increment by 1
+    //evaluate every number from 1 to 100
+    //i is each number being evaluated and increment by 1
     for (int i = 1; i < 101; i++) {
         
-        //print the number being evaluated
-        printf("%d\n", i);
-        
-        //evaluate each number divisible by 3
+        //evaluate for the remainder of the value divided by 3
+        //remainder is 0 if divisible by 3
         j = i % 3;
         
-        //evaluate each number divisible by 5
+        //evaluate for the remainder of the value divided by 5
+        //remainder is 0 if divisible by 5
         k = i % 5;
         
         //if a number is divisible by 3 but not 5, print "Fizz"
         if (j == 0 && k != 0) {
-            printf("Fizz\n");
+            printf("Fizz");
         }
         
-        //if a number is divisible by 5 but not 3, print "Buzz"
-        if (j != 0 && k == 0) {
-            printf("Buzz\n");
-        }
+            //if a number is divisible by 5 but not 3, print "Buzz"
+            else if (j != 0 && k == 0) {
+                printf("Buzz");
+            }
         
-        //if a number is divisible by 3 and 5, print "FizzBuzz"
-        if (j == 0 && k == 0) {
-            printf("FizzBuzz\n");
-        }
+            //if a number is divisible by 3 and 5, print "FizzBuzz"
+            else if (j == 0 && k == 0) {
+                printf("FizzBuzz");
+            }
+        
+            //if a number is neither divisible by 3 nor 5, print the number being evaluated
+            else {
+                printf("%d ", i);
+            }
+        
+        //print next line
+        printf("\n");
     }
-    
 }
